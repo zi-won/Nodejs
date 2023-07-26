a33928a3ddfec4538f833e876b26aa15267e01a4
지원 <88700479+zi-won@users.noreply.github.com>
2023-07-26 17:25:42 +0900

    cookie를 활용한 장바구니 구현

--- app_cookie.js
+++ app_cookie.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var cookieParser = require('cookie-parser'); // cookie-parser 모듈을 변수에 담음
 var app = express();
-app.use(cookieParser());
+app.use(cookieParser('12121212@@')); // 안에 들어있는 값은 키 (열쇠) 값임. 암호화된 값을 쿠키에 굽게된다는
 
 var products = { // DB 대용으로 사용
     1:{title:'The gistory of web 1'},
@@ -26,8 +26,8 @@ cart = {
 */
 app.get('/cart/:id', function(req, res) {
     var id = req.params.id; // parameter를 받아와야 함
-    if(req.cookies.cart) {
-        var cart = req.cookies.cart;
+    if(req.signedCookies.cart) {
+        var cart = req.signedCookies.cart;
     }
     else {
         var cart = {}; // 비어있는 최초의 값이 cookie의 값이 되는 것
@@ -36,7 +36,7 @@ app.get('/cart/:id', function(req, res) {
         cart[id] = 0;
     }
     cart[id] = parseInt(cart[id]) + 1; // 기본형이 문자형이므로 parseInt를 통해 강제로 숫자 형 변환을 함
-    res.cookie('cart', cart); // cookie에 cart라는 이름으로 cart의 변수에 들어있는 객체를 넣어줌
+    res.cookie('cart', cart, {signed:true}); // cookie에 cart라는 이름으로 cart의 변수에 들어있는 객체를 넣어줌
     res.redirect('/cart'); // cart에 담겨진 목록으로 사용자를 redirect(다른 URL로 재요청하라고 지시하는 것)
 });
 app.get('/cart', function(req, res) {
@@ -57,14 +57,14 @@ app.get('/cart', function(req, res) {
 });
 
 app.get('/count', function(req, res) {
-    if(req.cookies.count) { // cookie의 값이 있다면
-        var count = parseInt(req.cookies.count); // count에 쿠키의 값 넣기
+    if(req.signedCookies.count) { // cookie의 값이 있다면, signedcookies는 키 값으로 해독해주는 역할
+        var count = parseInt(req.signedCookies.count); // count에 쿠키의 값 넣기
     }
     else {
         var count = 0; // cookie의 값이 없다면 0으로 초기화
     }
     count = count + 1;
-    res.cookie('count', count); // cookie-parser로 인해서 cookie라는 메소드가 생김
+    res.cookie('count', count, {signed:true}); // cookie-parser로 인해서 cookie라는 메소드가 생김
     res.send('count : ' + req.cookies.count); // 웹브라우저가 웹 서버에게 전송한 쿠키 값으로 바뀌게 됨
 })
 app.listen(3003, function() {
