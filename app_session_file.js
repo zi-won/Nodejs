6af6077f018230400b4549d15213fcdd814fb9f5
지원 <88700479+zi-won@users.noreply.github.com>
2023-07-26 18:51:27 +0900

    session를 활용해 로그인과 로그아웃 구현, session data를 file에 저장해보기

---
+++ app_session_file.js
@@ -0,0 +1,84 @@
+var express = require('express');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+    secret: '1234abcdef!@#$%^',
+    resave: false,
+    saveUninitialized: true,
+    store : new FileStore()
+}));
+
+app.get('/count', function (req, res) {
+    if (req.session.count) {
+        req.session.count++;
+    } else {
+        req.session.count = 1;
+    }
+    res.send('count : ' + req.session.count);
+});
+app.get('/auth/logout', function(req, res) {
+    delete res.session.displayName;
+    res.redirect('/welcome');
+});
+app.get('/welcome', function (req, res) {
+    if(req.session.displayName) {
+        res.send(`
+            <h1>Hello, ${req.session.displayName}</h1>
+            <a href = "/auth/login">logout</a>
+        `)
+    }
+    else {
+        res.send(`
+            <h1>Welcome</h1>
+            <a href = "/auth/login">Login</a>
+        `)
+    }
+});
+
+app.post('/auth/login', function (req, res) {
+    var user = {
+        username: 'jiwon',
+        password: '111',
+        displayName: 'Jiwon'
+    };
+    var uname = req.body.id;
+    var pwd = req.body.password;
+    if (uname === user.username && pwd === user.password) {
+        req.session.displayName = user.displayName;
+        res.redirect('/welcome');
+    } else {
+        res.send('Who are you? <a href="/auth/login">login</a>');
+    }
+});
+
+app.get('/auth/login', function (req, res) {
+    var output = `
+        <h1>Login</h1>
+        <form action="/auth/login" method="post">
+            <p>
+                <input type="text" name="id" placeholder="username">
+            </p>
+            <p>
+                <input type="password" name="password" placeholder="password">
+            </p>
+            <p>
+                <input type="submit">
+            </p>
+        </form>
+    `;
+    res.send(output);
+});
+
+app.get('/auth/logout', function (req, res) {
+    delete req.session.displayName;
+    res.redirect('/welcome');
+});
+
+app.listen(3003, function () {
+    console.log('Connected 3003 port!!!');
+});
